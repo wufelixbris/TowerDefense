dfa1889728cc7ce49d835cb809cf4670af3743c0
wufelixbris <fw16243@my.bristol.ac.uk>
2017-04-30 02:40:08 +0100

    30/4:0239

--- src/com/felixwu/td/Config.java
+++ src/com/felixwu/td/Config.java
@@ -8,4 +8,13 @@ public class Config {
     public static final int BLOCK_SIZE = 40;
     public static final int MAP_HEIGHT = 7;
     public static final int MAP_WIDTH = 10;
+
+    // HP, Damage, Gold settings
+    public static final int BULLET_DAMAGE = 1;
+    public static final int ENEMY_DAMAGE = 1;
+    public static final int ENEMY_HP = 2;
+    public static final int HOME_HP = 5;
+    public static final int START_GOLD = 200;
+    public static final int KILL_GOLD = 20;
+    public static final int TOWER_GOLD = -100;
 }
